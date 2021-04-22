0a36e8509460cc1879e59fd4b96e7534d32b786d
Ashley Anderton <ashley.j.anderton@gmail.com>
2021-04-22 15:39:38 +0100

    First attempt at stealing calendar stuff

--- .dev/gulp/tasks/default.js
+++ .dev/gulp/tasks/default.js
@@ -37,6 +37,14 @@ gulp.task('customjs', () => {
         .pipe(gulp.dest(paths.dist.Assets));
 });
 
+gulp.task('calendarjs', () => {
+    return src([
+        'Frontend/src/js/tavo-calendar.js',
+    ])
+        .pipe(gulp.dest(paths.dist.Assets));
+});
+
+
 gulp.task('sass', () => {
         return src(paths.src.SCSS)
             .pipe(wait(200))
