80e133350c51cd484660ec476827fa20a88cfbf6
Ashley Anderton <ashley.j.anderton@gmail.com>
2021-05-17 15:27:28 +0100

    More details js Stuff

--- assets/custom.js
+++ assets/custom.js
@@ -49,6 +49,8 @@ $(document).ready(function () {
 
 $(".govuk-tabs").attr("data-module", "govuk-tabs");
 $(".govuk-details").attr("data-module", "govuk-details");
+$(".govuk-details__summary").click(this.parent(".govuk-details").attr("open"));
+
 
 
 
