387e2c953671be7745448b4edfee6732738c5cc8
Mike Wild <wildconsultingltd@googlemail.com>
2021-11-08 14:44:08 +0000

    Move polyfill to before calendar code

--- assets/custom.js
+++ assets/custom.js
@@ -292,6 +292,58 @@ if (cookieConsent.length) {
 }
 /* Cookie Article, with consent ends */
 
+//polyfill for IE11
+if (!Array.prototype.includes) {
+    console.log("Running polyfill for Array.includes");
+    Object.defineProperty(Array.prototype, 'includes', {
+        value: function (searchElement, fromIndex) {
+
+        if (this == null) {
+            throw new TypeError('"this" is null or not defined');
+        }
+
+        // 1. Let O be ? ToObject(this value).
+        var o = Object(this);
+
+        // 2. Let len be ? ToLength(? Get(O, "length")).
+        var len = o.length >>> 0;
+
+        // 3. If len is 0, return false.
+        if (len === 0) {
+            return false;
+        }
+
+        // 4. Let n be ? ToInteger(fromIndex).
+        //    (If fromIndex is undefined, this step produces the value 0.)
+        var n = fromIndex | 0;
+
+        // 5. If n ≥ 0, then
+        //  a. Let k be n.
+        // 6. Else n < 0,
+        //  a. Let k be len + n.
+        //  b. If k < 0, let k be 0.
+        var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+
+        function sameValueZero(x, y) {
+            return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
+        }
+
+        // 7. Repeat, while k < len
+        while (k < len) {
+            // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+            // b. If SameValueZero(searchElement, elementK) is true, return true.
+            if (sameValueZero(o[k], searchElement)) {
+            return true;
+            }
+            // c. Increase k by 1.
+            k++;
+        }
+
+        // 8. Return false
+        return false;
+        }
+    });
+}
 
 /* APPROVED USER CHECK */
 
@@ -682,57 +734,6 @@ function frontDoor(apiKey, dataSource) {
 }
 /* END front door functions */
 
-//polyfill for IE11
-if (!Array.prototype.includes) {
-    Object.defineProperty(Array.prototype, 'includes', {
-        value: function (searchElement, fromIndex) {
-
-        if (this == null) {
-            throw new TypeError('"this" is null or not defined');
-        }
-
-        // 1. Let O be ? ToObject(this value).
-        var o = Object(this);
-
-        // 2. Let len be ? ToLength(? Get(O, "length")).
-        var len = o.length >>> 0;
-
-        // 3. If len is 0, return false.
-        if (len === 0) {
-            return false;
-        }
-
-        // 4. Let n be ? ToInteger(fromIndex).
-        //    (If fromIndex is undefined, this step produces the value 0.)
-        var n = fromIndex | 0;
-
-        // 5. If n ≥ 0, then
-        //  a. Let k be n.
-        // 6. Else n < 0,
-        //  a. Let k be len + n.
-        //  b. If k < 0, let k be 0.
-        var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
-
-        function sameValueZero(x, y) {
-            return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
-        }
-
-        // 7. Repeat, while k < len
-        while (k < len) {
-            // a. Let elementK be the result of ? Get(O, ! ToString(k)).
-            // b. If SameValueZero(searchElement, elementK) is true, return true.
-            if (sameValueZero(o[k], searchElement)) {
-            return true;
-            }
-            // c. Increase k by 1.
-            k++;
-        }
-
-        // 8. Return false
-        return false;
-        }
-    });
-}
 // Based on https://github.com/alphagov/govuk_publishing_components/blob/v22.0.0/app/assets/javascripts/govuk_publishing_components/components/step-by-step-nav.js
 
 /* eslint-env jquery */
